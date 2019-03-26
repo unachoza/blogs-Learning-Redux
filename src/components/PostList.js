d1d71060a661cb694036c52dbd10c57df313e414
Arianna Choza <unachoza@gmail.com>
2019-03-25 22:03:16 -0700

    setting up react and redux and semantic ui

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+
+class PostList extends Component{
+    render() {
+        return (
+            <div>Post List</div>
+        )
+    }
+}
+export default PostList
